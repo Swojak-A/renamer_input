51419fb8381bf16fb126aea3e68ef71aebaafc5e
SwA <swojak.a@gmail.com>
2017-05-21 17:19:03 +0200

    renamer_input ver. 0.1.2
    working version of renamer
    
    done:
    - dict containing path and isfile/isdir output
    - rename() and file_count() merged to one method swipe_dict()
    - check_file() refactored
    
    to do list:
    - add function ask_to_rename_folder()
    - OOP

--- renamer_input.py
+++ renamer_input.py
@@ -1,35 +1,57 @@
 import os
 
 relative_path = "files"
-relative_path_full = relative_path + "/"
+
+modify_directory_names = False
 
 def rename(file, phrase_to_change, changed_phrase):
     print("Found the file: " + file)
-    new_file_name = file.replace(phrase_to_change, changed_phrase)
+    new_file_name = file[:file.rfind("/") + 1] + file[file.rfind("/") + 1:].replace(phrase_to_change, changed_phrase)
     print("Renaming to: " + new_file_name)
     os.rename(file, new_file_name)
     print("Success! File renamed!\n")
 
-def check_file(file, relative_path_full, phrase_to_change):
-    if os.path.isfile(relative_path_full + file) == True and phrase_to_change in file:
-        return True
+item_dict = {}
 
-def item_dict(relative_path):
+def create_item_dict(relative_path):
     relative_path_full = relative_path + "/"
     for item in os.listdir(relative_path):
-        print(print(relative_path_full + file))
+        #print(relative_path_full + item)
+        if os.path.isdir(relative_path_full + item):
+            item_dict[relative_path_full + item] = "directory"
+            create_item_dict(relative_path_full + item)
+        else:
+            item_dict[relative_path_full + item] = "file"
 
-
-def file_count(relative_path, phrase_to_change):
+def file_count(dict):
     count = 0
-    relative_path_full = relative_path + "/"
-    for file in os.listdir(relative_path):
-        print(relative_path_full + file)
-        if check_file(file, relative_path_full, phrase_to_change) == True:
-            count += 1
-        elif os.path.isdir(relative_path_full + file):
-            count += file_count(relative_path_full + file, phrase_to_change)
+    for e in dict:
+        if dict[e] == "file":
+            if phrase_to_change in e[e.rfind("/") + 1:]:
+                count += 1
+        elif modify_directory_names == True:
+            if dict[e] == "directory":
+                if phrase_to_change in e[e.rfind("/") + 1:]:
+                    count += 1
     return count
+
+def swipe_dict(dict, func=None):
+    count = 0
+    for e in dict:
+        if dict[e] == "file":
+            if phrase_to_change in e[e.rfind("/") + 1:]:
+                if func is not None:
+                    func(e, phrase_to_change, changed_phrase)
+                count += 1
+        elif modify_directory_names == True:
+            if dict[e] == "directory":
+                if phrase_to_change in e[e.rfind("/") + 1:]:
+                    if func is not None:
+                        func(e, phrase_to_change, changed_phrase)
+                    count += 1
+    return count
+
+
 
 print(
 """
@@ -41,24 +63,27 @@ PLEASE FOLLOW THE INSTRUCTIONS
 #1. asking for right placement of files
 #input('1. Put the files you want to rename in the "/files" directory in the renamer_input folder and press Enter to continue\n')
 
-print("We found the following files: \n" + str(os.listdir(relative_path)) + "\n")
+create_item_dict(relative_path)
+print(item_dict)
 
-# #2. getting phrase that user want to be changed
-# phrase_to_change = input("2. Type a phrase you want to change and press Enter: \n")
-#
-# #3. checking if there are files that qualify to be changed and then renaming them
-# count = file_count(relative_path, phrase_to_change)
-#
-# if count > 0:
-#     print("There is %s files matching criteria \n" % count)
-#
-#     changed_phrase = input("3. Type phrase intended to replace phrase you wanted to change and press Enter: \n")
-#
-#     for file in os.listdir(relative_path):
-#         if check_file(file, relative_path_full, phrase_to_change) == True:
-#             rename(relative_path_full + file, phrase_to_change, changed_phrase)
-#     print("No more files matching criteria \n")
-# else:
-#     print("\nNo files matching criteria \n")
-#
-# input("Press Enter to close the program")
+#print("We found the following files: \n" + str(item_dict(relative_path)) + "\n")
+
+#2. getting phrase that user want to be changed
+phrase_to_change = input("2. Type a phrase you want to change and press Enter: \n")
+
+#3. checking if there are files that qualify to be changed and then renaming them
+count = swipe_dict(item_dict)
+print(count)
+
+if count > 0:
+    print("There is %s files matching criteria \n" % count)
+
+    changed_phrase = input("3. Type phrase intended to replace phrase you wanted to change and press Enter: \n")
+
+    swipe_dict(item_dict, rename)
+
+    print("No more files matching criteria \n")
+else:
+    print("\nNo files matching criteria \n")
+
+input("Press Enter to close the program")
