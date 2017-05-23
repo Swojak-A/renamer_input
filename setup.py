dcdbe2ba25b6fb37c91ecae070a084bcd3ca5dbf
SwA <swojak.a@gmail.com>
2017-05-23 11:43:19 +0200

    renamer_input ver. 0.1.3
    working version of renamer
    
    done:
    - added distribution
    
    to do list:
    - OOP

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['renamer_input.py'])
+Mydata_files = [('files', [])]
+
+setup(
+    console=['renamer_input.py'],
+    data_files = Mydata_files,
+    options={
+                "py2exe":{
+                        "unbuffered": True,
+                        "optimize": 2,
+                        "excludes": ["email"]
+                }
+        }
+)
